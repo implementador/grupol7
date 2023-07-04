4225f518c7a753079ec1038c00b4b50c3805f76c
Lava Studio <86937922+LavaStudioMx@users.noreply.github.com>
2023-07-04 09:06:30 -0600

    Add files via upload

--- purchase_discount_app/__manifest__.py
+++ purchase_discount_app/__manifest__.py
@@ -3,8 +3,8 @@
 {
     'name': 'Discount on Purchase Order',
     "author": "Edge Technologies",
-    'version': '16.0.1.0',
-    'live_test_url': "https://youtu.be/OMgFWc1ik2Y",
+    'version': '16.0.1.1',
+    'live_test_url': "",
     "images":['static/description/main_screenshot.png'], 
     'summary': 'Purchase discount on purchase order line discount on purchase order line apply discount on purchase order lines apply discount on purchase line print discount on purchase order',
     'description': 'Purchase discount on purchase order line discount on purchase order line apply discount on purchase order lines apply discount on purchase line print discount on purchase order',
