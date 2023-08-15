36a1c99845ee2967ad8a7fd5f67eae47e6aca6c6
Lava Studio <86937922+LavaStudioMx@users.noreply.github.com>
2023-08-15 15:03:08 -0600

    Update BiProductScreen.js

--- bi_pos_pay_later/static/src/js/BiProductScreen.js
+++ bi_pos_pay_later/static/src/js/BiProductScreen.js
@@ -11,7 +11,7 @@ odoo.define('bi_pos_pay_later.BiProductScreen', function(require) {
                 this.env.pos.add_new_order();
             }
             const product = event.detail;
-            if(product.qty_available > -1){
+            if(product.qty_available > -10){
 
 	            const options = await this._getAddProductOptions(product);
 	            // Do not add product if options is undefined.
