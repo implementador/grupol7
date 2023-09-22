9d1694917a94442960a84cc6a6a1db30573d90b7
Lava Studio <86937922+LavaStudioMx@users.noreply.github.com>
2023-09-22 11:43:54 -0600

    Update bi_pos_stock.py

--- bi_pos_stock/models/bi_pos_stock.py
+++ bi_pos_stock/models/bi_pos_stock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+#LavaStudio
 
 from odoo import fields, models, api, _
 from odoo.exceptions import Warning
