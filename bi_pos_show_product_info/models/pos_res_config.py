0b8e57a7c73acdf4a2cfd16eb8f5fbf14fa527ce
Lava Studio <86937922+LavaStudioMx@users.noreply.github.com>
2024-02-05 13:26:40 -0600

    Update pos_res_config.py

--- bi_pos_show_product_info/models/pos_res_config.py
+++ bi_pos_show_product_info/models/pos_res_config.py
@@ -7,7 +7,7 @@ from odoo import fields, models, api, _
 class ResConfigSettings(models.TransientModel):
 	_inherit = 'res.config.settings'
 
-	pos_show_product_info = fields.Boolean(related='pos_config_id.show_product_info', readonly=False)
-	pos_hide_cost_currency = fields.Boolean(related='pos_config_id.hide_cost_currency', readonly=False)
-	pos_hide_margin = fields.Boolean(related='pos_config_id.hide_margin', readonly=False)
-	pos_show_available_pricelist_ids = fields.Many2many(related='pos_config_id.show_available_pricelist_ids', readonly=False)
+	pos_show_product_info = fields.Boolean(related='pos_config_id.show_product_info', string='Mostrar información de producto', readonly=False)
+	pos_hide_cost_currency = fields.Boolean(related='pos_config_id.hide_cost_currency', string='Ocultar costo', readonly=False)
+	pos_hide_margin = fields.Boolean(related='pos_config_id.hide_margin', string='Ocultar margen', readonly=False)
+	pos_show_available_pricelist_ids = fields.Many2many(related='pos_config_id.show_available_pricelist_ids', string='Listas de precio a mostrar', readonly=False)
