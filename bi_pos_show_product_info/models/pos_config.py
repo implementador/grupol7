168d0df4f3ae4f447141b5c5368334b512e9f196
Lava Studio <86937922+LavaStudioMx@users.noreply.github.com>
2024-02-05 14:56:37 -0600

    Update pos_config.py

--- bi_pos_show_product_info/models/pos_config.py
+++ bi_pos_show_product_info/models/pos_config.py
@@ -10,4 +10,4 @@ class PosConfig(models.Model):
     show_product_info = fields.Boolean(string="Mostrar información de producto")
     hide_cost_currency = fields.Boolean(string="Ocultar costo")
     hide_margin = fields.Boolean(string="Ocultar margen")
-    show_available_pricelist_ids = fields.Many2many('product.pricelist', 'show_available_pricelist_rel', 'available_pro_pricelist', 'pro_pricelist', string="Listas de precio a mostrar",store="True")
+    show_available_pricelist_ids = fields.Many2many('product.pricelist', 'show_available_pricelist_rel', 'available_pro_pricelist', 'pro_pricelist', string='Listas de precio a mostrar', store='True')
