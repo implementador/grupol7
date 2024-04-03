a575c4b866cf474af0e0e9eb22f508f95f12f603
Lava Studio <86937922+LavaStudioMx@users.noreply.github.com>
2024-04-03 01:26:33 -0600

    Update __manifest__.py

--- product_tax_multicompany_default/__manifest__.py
+++ product_tax_multicompany_default/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2019 Tecnativa - Carlos Dauden
+# Copyright 2017-2019 Pymex ERP
 # Copyright 2018 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
